228cc0d19d20148b42e68c1d5813c2924d583f5e
rood-red <vfpbillones@gmail.com>
2024-03-15 07:20:58 +0800

    rood-red

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'fooderlich_theme.dart';
+import 'home.dart';
+void main() {
+  runApp(const Fooderlich());
+}
+
+class Fooderlich extends StatelessWidget {
+  const Fooderlich({super.key});
+  @override
+  Widget build(BuildContext context) {
+    // 2
+    final theme = FooderlichTheme.dark();
+    // TODO: Apply Home widget
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+ theme: theme,
+ title: 'Fooderlich',
+ home: const Home(),
+);
+  }
+}
